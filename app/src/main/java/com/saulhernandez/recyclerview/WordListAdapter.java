dc8452e9b9c882f59f7f375c4c6212c1ac3e73ab
saulh <saulhernandez217@gmail.com>
2021-06-20 21:46:48 -0700

    Finished WordListAdapter

--- app/src/main/java/com/saulhernandez/recyclerview/WordListAdapter.java
+++ app/src/main/java/com/saulhernandez/recyclerview/WordListAdapter.java
@@ -1,5 +1,7 @@
 package com.saulhernandez.recyclerview;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -7,22 +9,36 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.LinkedList;
+
 public class WordListAdapter extends RecyclerView.Adapter<WordListAdapter.WordViewHolder> {
+
+    private final LinkedList<String> mWordList;
+    private LayoutInflater mInflator;
+
+    public WordListAdapter(Context context, LinkedList<String> mWordList) {
+        this.mInflator = LayoutInflater.from(context); // how would we know to do this???
+        this.mWordList = mWordList;
+    }
 
     @NonNull
     @Override
-    public WordListAdapter.WordViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+    public WordListAdapter.WordViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // why attachToRoot: false???
+        View mItemView = mInflator.inflate(R.layout.wordlist_item, parent, false);
+
+        return new WordViewHolder(mItemView, this);
     }
 
     @Override
-    public void onBindViewHolder(WordListAdapter.WordViewHolder holder, int position) {
-
+    public void onBindViewHolder(@NonNull WordListAdapter.WordViewHolder holder, int position) {
+        String mCurrent = mWordList.get(position);
+        holder.wordItemView.setText(mCurrent);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mWordList.size();
     }
 
     class WordViewHolder extends RecyclerView.ViewHolder {
